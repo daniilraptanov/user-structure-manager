765fbb47103cd8c1c3668098cc099b3cbb3a946a
Daniil Raptanov <daniilraptanov@gmail.com>
2022-07-19 13:29:22 +0300

    auth routes

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,23 +1,50 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-
+import User from "../db/models/user";
+import { AuthServiceImpl } from "../services/auth-service";
+import { ILoginDTO } from "../types/dto/auth-dto";
+import { IUserDTO } from "../types/dto/user-dto";
+import { IUser } from "../types/models/user";
 
 export class AuthController {
-    static async login(req: Request, res: Response) {
-        try {
-            
-        } catch (err) {
-            console.log(err);
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
-        }
-    }
+  static async login(req: Request, res: Response) {
+    try {
+      const authService = new AuthServiceImpl();
 
-    static async register(req: Request, res: Response) {
-        try {
-            
-        } catch (err) {
-            console.log(err);
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
-        }
+      const data: ILoginDTO = req.body;
+      const user: IUser = await User.findOne().where({ login: data.login });
+
+      const token = authService.createToken(user.id, user.role);
+
+      if (!token) {
+        return res.status(500).send("Token does not created");
+      }
+
+      res.status(200).json({ token: token });
+    } catch (err) {
+      console.log(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
     }
+  }
+
+  static async register(req: Request, res: Response) {
+    try {
+      const authService = new AuthServiceImpl();
+
+      const data: IUserDTO = req.body;
+      const user: IUser = await new User({
+        ...data,
+        password: await authService.hashedPassword(data.password),
+      });
+
+      if (!user) {
+        res.status(500).send("User does not created!");
+      }
+
+      res.status(201).send("User was created");
+    } catch (err) {
+      console.log(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
+    }
+  }
 }
--- src/types/dto/user-dto.ts
+++ src/types/dto/user-dto.ts
@@ -2,7 +2,7 @@ import { Role } from "../enums/role-enum";
 import { ILoginDTO } from "./auth-dto";
 
 export interface IUserDTO extends ILoginDTO {
-    id: string;
+    id?: string;
     role: Role;
     bossId: string;
 }
